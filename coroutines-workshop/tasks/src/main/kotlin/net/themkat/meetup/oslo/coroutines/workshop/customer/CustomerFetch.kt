9b51a0c5757305d38c07ab0a43311461c353e894
jarlehansen <jarle@jarlehansen.net>
2019-03-26 15:48:08 +0100

    print customer id

--- coroutines-workshop/tasks/src/main/kotlin/net/themkat/meetup/oslo/coroutines/workshop/customer/CustomerFetch.kt
+++ coroutines-workshop/tasks/src/main/kotlin/net/themkat/meetup/oslo/coroutines/workshop/customer/CustomerFetch.kt
@@ -4,5 +4,6 @@ import org.apache.commons.lang3.RandomStringUtils
 
 fun fetchCustomer(id: Int): Customer {
     Thread.sleep(1000)
+    println("Fetching customer: $id")
     return Customer(id, RandomStringUtils.randomAlphabetic(15), (0..10000).random())
 }
